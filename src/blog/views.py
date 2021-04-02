5d74484007b9628d9bd6223e54606cd64bda8a62
Andre Perez <andremztm@gmail.com>
2021-04-01 21:52:35 -0300

    Home page and article details

--- src/blog/views.py
+++ src/blog/views.py
@@ -6,3 +6,9 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+    
