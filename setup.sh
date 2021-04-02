557abb3a37bdb88863d9691ad28d7a9132f8eda5
Andre Perez <andremztm@gmail.com>
2021-04-02 18:44:49 -0300

    Installation process

--- setup.sh
+++ setup.sh
@@ -5,10 +5,30 @@ touch ./.env
 #This will generate the project environment variables via user's input
 echo "Choose the name of your database:"
 read POSTGRES_DB
+
+if [ -z "$POSTGRES_DB" -eq 0 ]
+  then
+    echo "Database name must not be empty. Please input a valid name."
+    exit 1
+fi
+
 echo "Choose your database username:"
 read POSTGRES_USER
+
+if [ -z "$POSTGRES_USER" -eq 0 ]
+  then
+    echo "Database username must not be empty. Please input a valid name"
+    exit 1
+fi
+
 echo "Choose your database password:"
 read POSTGRES_PASSWORD
+
+if [ -z "$POSTGRES_PASSWORD" -eq 0 ]
+  then
+    echo "Database password must not be empty. Please choose a valid password"
+    exit 1
+fi
 
 #Saving the variables at .env file
 echo "POSTGRES_DB=$POSTGRES_DB" >> ./.env
@@ -17,4 +37,3 @@ echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> ./.env
 
 #Building docker-compose.yml
 docker-compose up --build -d
-
--- src/core/settings.py
+++ src/core/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+
 from pathlib import Path
+from secret_key_generator import secret_key_generator
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = secret_key_generator.generate()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
