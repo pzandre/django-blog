1c0bab4e62f0ad1e937dd485f1ead18346b83906
Andre Perez <andremztm@gmail.com>
2021-04-02 12:19:53 -0300

    Installation script

---
+++ src/core/setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#This will generate the project environment variables via user's input
+echo "Choose the name of your database:"
+read POSTGRES_DB
+echo "Choose your database username:"
+read POSTGRES_USER
+echo "Choose your database password:"
+read POSTGRES_PASSWORD
+
+#Saving the variables at .env file
+echo "POSTGRES_DB=$POSTGRES_DB" >> ./.env
+echo "POSTGRES_USER=$POSTGRES_USER" >> ./.env
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> ./.env
+
+#Building docker-compose.yml
+docker-compose up --build -d
+
