7a40f8e202fa2c416f78266df75e5f634841a606
Andre Perez <andremztm@gmail.com>
2021-04-02 18:47:24 -0300

    Installation process

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ touch ./.env
 echo "Choose the name of your database:"
 read POSTGRES_DB
 
-if [ -z "$POSTGRES_DB" -eq 0 ]
+if [ -z "$POSTGRES_DB" ]
   then
     echo "Database name must not be empty. Please input a valid name."
     exit 1
@@ -15,7 +15,7 @@ fi
 echo "Choose your database username:"
 read POSTGRES_USER
 
-if [ -z "$POSTGRES_USER" -eq 0 ]
+if [ -z "$POSTGRES_USER" ]
   then
     echo "Database username must not be empty. Please input a valid name"
     exit 1
@@ -24,7 +24,7 @@ fi
 echo "Choose your database password:"
 read POSTGRES_PASSWORD
 
-if [ -z "$POSTGRES_PASSWORD" -eq 0 ]
+if [ -z "$POSTGRES_PASSWORD" ]
   then
     echo "Database password must not be empty. Please choose a valid password"
     exit 1
