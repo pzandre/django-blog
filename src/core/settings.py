5e8c8bb98dab42efb7b5c1d10df97a19acd4d017
Andre Perez <andremztm@gmail.com>
2021-04-06 19:24:47 -0300

    Updating settings.py with new django 3.2 fields

--- src/core/settings.py
+++ src/core/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
