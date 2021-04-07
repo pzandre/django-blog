853ad466b168aaa5c4fa882d455ddd5383ca0aaa
Andre Perez <andremztm@gmail.com>
2021-04-07 13:49:32 -0300

    Post dates

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     tags = models.CharField(max_length=255)
+    post_date = models.DateField(auto_now_add=True, default=django.utils.timezone.now)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
