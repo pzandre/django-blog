603b4f001b81ea6225fe1935fd5872130499fbe9
Andre Perez <andremztm@gmail.com>
2021-04-08 13:01:39 -0300

    Category added

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,13 +3,25 @@ from django.db import models
 from django.urls import reverse
 from datetime import datetime, date
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+    return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    preview = models.Charfield(max_length=255)
+    preview = models.CharField(max_length=255)
     body = models.TextField()
     tags = models.CharField(max_length=255)
     post_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
