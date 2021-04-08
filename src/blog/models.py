ce9f97cdd8e4d5f6a12142c8310397de30093acc
Andre Perez <andremztm@gmail.com>
2021-04-07 23:33:31 -0300

    Fixing post dates

--- src/blog/models.py
+++ src/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     tags = models.CharField(max_length=255)
-    post_date = models.DateField(auto_now_add=True, default=django.utils.timezone.now)
+    post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
