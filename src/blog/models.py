fa32dd788c94d46d5d8bc8a0971f86bf8ca1e2ba
Andre Perez <andremztm@gmail.com>
2021-04-13 09:56:56 -0300

    Rich Text Editor

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from colorfield.fields import ColorField
 from django.contrib.auth.models import User
 from django.db import models
@@ -25,11 +26,10 @@ class Post(models.Model):
     slug_url = models.SlugField(max_length=50)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     preview = models.CharField(max_length=255)
-    body = models.TextField()
+    body = models.RichTextField(blank=True, null=True)
     tags = models.CharField(max_length=255)
     post_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='blog_post_likes')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
